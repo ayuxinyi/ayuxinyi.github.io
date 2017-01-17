f3672e4fe3574dcc07548dd4b22557d78f4638de
ayuxinyi <1079049425@qq.com>
2017-01-17 20:53:11 +0800

    Update products.js

--- shop/js/products.js
+++ shop/js/products.js
@@ -2,7 +2,7 @@
 app.controller("productVC",function($scope,$http,$rootScope){
 	$http({
 		method:"GET",
-		url:"http://127.0.0.1/shop/php/product.php",
+		url:"http://ayuxinyi.github.io/shop/php/product.php",
 	}).then(function(response){
 //		console.log(response.data);
 		$scope.products = response.data;
